36ed1ac3459be2b3f3a5be132586d2b343bd89c9
Serial Lazer <mooniitk@gmail.com>
2018-11-24 23:45:54 +0800

    parameterizing stuff

--- src/config_file.py
+++ src/config_file.py
@@ -1,14 +1,14 @@
 import hashlib
-import sys
-
-print(sys.version)
+import os
 
 Months = dict(JAN='01', FEB='02', MAR='03', APR='04', MAY='05', JUN='06', JUL='07', AUG='08', SEP='09', OCT='10',
               NOV='11', DEC='12')
-old_category_file = '/Users/mayankgupta/Documents/Budget/Categories_all.csv'
-category_file = '/Users/mayankgupta/Documents/Budgetified/Categories_all_NEW.csv'
-uncategorized_file = '/Users/mayankgupta/Documents/Budgetified/Uncategorized_Transactions.csv'
-default_accounts_dir = '/Users/mayankgupta/Documents/Budgetified/Accounts/'
+
+home_dir = os.environ['HOME']
+old_category_file = home_dir + '/Documents/Budget/Categories_all.csv'
+category_file = home_dir + '/Documents/Budgetified/Categories_all_NEW.csv'
+uncategorized_file = home_dir + '/Documents/Budgetified/Uncategorized_Transactions.csv'
+default_accounts_dir = home_dir + '/Documents/Budgetified/Accounts/'
 archive_subdir = '/archive_files/'
 default_income_category = 'Income|Misc'
 default_expense_category = 'Expenses|Misc'
@@ -24,7 +24,6 @@ def get_default_category(val):
     if float(val) > 0:
         return default_income_category
     return default_expense_category
-
 
 def create_transaction_hash(transaction_detail_list):
     return hashlib.md5(str(transaction_detail_list).encode()).hexdigest()
